eb0eb6d877b4ff512188df7275c6066a2765db98
Kunal Dubey <142141622+kunaldubey10@users.noreply.github.com>
2026-01-13 15:44:17 +0530

    Delete HerbalTrace/start-backend-dedicated.ps1

--- start-backend-dedicated.ps1
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env pwsh
-# Start backend in dedicated process
-
-Set-Location "$PSScriptRoot\backend"
-Write-Host "Starting HerbalTrace Backend from: $PWD" -ForegroundColor Green
-npm start
